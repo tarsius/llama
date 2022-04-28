5d1da6dc68a1f76f52b7acc808ee430b72fb98ae
Jonas Bernoulli <jonas@bernoul.li>
2022-04-28 16:05:17 +0200

    Refresh library header

--- llama.el
+++ llama.el
@@ -1,24 +1,25 @@
-;;; llama.el --- Anonymous function literals       -*- lexical-binding: t -*-
+;;; llama.el --- Anonymous function literals  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2020-2022  Jonas Bernoulli
+;; Copyright (C) 2020-2022 Jonas Bernoulli
 
 ;; Authors: Jonas Bernoulli <jonas@bernoul.li>
-;; URL: https://git.sr.ht/~tarsius/llama
+;; Homepage: https://git.sr.ht/~tarsius/llama
 ;; Keywords: extensions
 
 ;; Package-Requires: ((seq "2.20"))
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation, either version 3 of the License,
 ;; or (at your option) any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
