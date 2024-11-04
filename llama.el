f4e80a582f7ec857783274bee6614560ce49d497
Jonas Bernoulli <jonas@bernoul.li>
2024-11-04 23:06:39 +0100

    Release version 0.4.0

--- llama.el
+++ llama.el
@@ -6,6 +6,7 @@
 ;; Homepage: https://github.com/tarsius/llama
 ;; Keywords: extensions
 
+;; Package-Version: 0.4.0
 ;; Package-Requires: ((emacs "26.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
